4d11f8776680acef89c29c8fd089bcbbf6870911
4222301031_wirdana <wirdanadana88@gmail.com>
2026-05-29 18:15:46 +0700

    Add files via upload

---
+++ week_1_introduction_computer_vision/main.py
@@ -0,0 +1,15 @@
+def penjumlahan(a, b):
+    """
+    Menghitung penjumlahan dari dua angka.
+    :param a: angka pertama
+    :param b: angka kedua
+    :return: hasil penjumlahan a + b
+    """
+    # TODO: Implementasikan fungsi ini
+    c = a + b
+    return c
+
+
+# Contoh penggunaan (dapat dihapus jika diinginkan)
+if __name__ == "__main__":
+    print(penjumlahan(3, 5))
